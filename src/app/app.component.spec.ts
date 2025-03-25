1e1674c70c0ad77582204571d9c41dab9180b76b
dg-gustavo <162385441+gusbitnetwork@users.noreply.github.com>
2025-03-25 15:25:08 -0600

    update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pokemons23'`, () => {
+  it(`should have as title 'pokemon-api'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pokemons23');
+    expect(app.title).toEqual('pokemon-api');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('pokemons23 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('pokemon-api app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'pokemons23';
+  title = 'pokemon-api';
 }
